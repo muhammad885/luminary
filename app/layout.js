a66ce4b4348468a84c3bb42ea7815300342042d6
Muhammad Ibrahim <muhammadibrahimmanofgod@gmail.com>
2025-06-19 15:02:26 -0700

    Fix PostCSS config

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export default async function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SessionProvider session={session} key={session?.user?.id ?? 'Customer'}>
+        <SessionProvider session={session} key={session?.user?.id}>
           <Providers>
             {children}
           </Providers>
