1042a581de6081772060abe8aa62517110890ed1
Muhammad Ibrahim <muhammadibrahimmanofgod@gmail.com>
2025-06-22 04:13:32 -0700

    new big changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'randomuser.me',
-        pathname: '/api/portraits/**',
-      },
-    ],
-  },
+ eslint: {
+  ignoreDuringBuilds: true
+ }
 };
 
 export default nextConfig;
