10a2812e0aaee0113276591313945794e32dcf93
Muhammad Ibrahim <muhammadibrahimmanofgod@gmail.com>
2025-06-22 06:35:15 -0700

    kai

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,8 @@
 module.exports = {
-  plugins: [
-    require('postcss-import')(),
-    require('postcss-flexbugs-fixes'),
-    require('postcss-preset-env')({
-      autoprefixer: {
-        flexbox: 'no-2009',
-      },
-      stage: 3,
-    }),
-    require('postcss-nested')(),
-    require('tailwindcss')(),
-    require('autoprefixer')(),
-    ...(process.env.NODE_ENV === 'production'
-      ? [require('cssnano')({ preset: 'default' })]
-      : [])
-  ]
+  plugins: {
+    'postcss-import': {},     // Handles @import if needed
+    'tailwindcss': {},
+    'postcss-nested': {},     // Handles nesting like in your globals.css
+    'autoprefixer': {},
+  },
 }
